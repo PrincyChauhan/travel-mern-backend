dfac87be8a4fc13c7837ffec427ba22fa21ed292
PrincyChauhan <chauhanprincee7@gmail.com>
2023-03-25 16:11:32 +0530

    Add post controller for place

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const placesRoutes = require("./routes/places-routes");
+
 const app = express();
 
+app.use(bodyParser.json());
 app.use("/api/places", placesRoutes);
+
+
 app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
