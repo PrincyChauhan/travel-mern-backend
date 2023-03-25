75e63dd636e497776a740df0a2a86ebaaca22aa1
PrincyChauhan <chauhanprincee7@gmail.com>
2023-03-25 14:44:35 +0530

    Error Handling

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ const bodyParser = require("body-parser");
 const placesRoutes = require("./routes/places-routes");
 const app = express();
 
-app.use('/api/places',placesRoutes)
+app.use("/api/places", placesRoutes);
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occured!" });
+});
 app.listen(3000, () => {
   console.log("listening on 3000");
 });
