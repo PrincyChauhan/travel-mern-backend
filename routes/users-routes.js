a9cb0bbcc2d6550b3886d66fac1afd381b413a71
PrincyChauhan <chauhanprincee7@gmail.com>
2023-03-30 21:13:55 +0530

    Validating Patch route request and user route

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const { check } = require("express-validator");
+
 const usersControllers = require("../controllers/users-controller");
 const router = express.Router();
 
 router.get("/", usersControllers.getUser);
-router.post("/signup", usersControllers.signup);
+router.post(
+  "/signup",
+  [
+    check("name").not().isEmpty(),
+    check("email").normalizeEmail().isEmail(),
+    check("password").isLength({ min: 6 }),
+  ],
+  usersControllers.signup
+);
 router.post("/login", usersControllers.login);
 module.exports = router;
